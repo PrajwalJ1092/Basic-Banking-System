782b5d5f7759aee54161809438d7fdda1e706428
PrajwalJ1092 <128353387+PrajwalJ1092@users.noreply.github.com>
2023-03-21 20:55:07 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,21 @@
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+
+</head>
+<center><div class="card text-white bg-secondary mb-3" style="font-family: 'Audiowide', sans-serif; max-width: 40rem; max-height: 450; opacity: 0.8;">
+ 
+  <div class="card-header" ><h2><u>ABOUT ME:</u></h2></div>
+  <h3>NAME : PRAJWAL JAINKERI</h3><br>
+  <h4>COLLEGE : Gogte Institute Of Technology</h4><br>
+  <h4>DEPARTMENT : Information Science Engineering</h4>
+  <h4>.</h4>
+  <h4>.</h4>
+  <?php 
+  include "socialmediapg.php";
+  ?>
+
+
+
+ </div> 
+</div>
+</center>
